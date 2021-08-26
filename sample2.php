b90198eae73fae13c02dc22ff655f9ba52b6c531
marumyon <yumenoxx0313@gmail.com>
2021-08-26 22:29:48 +0900

    main

---
+++ sample2.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="sample1-2.css">
+</head>
+<body>
+  //htmlに埋め込むとき<?php～?>の中にphpのコードを書く
+<?php
+    //echoは出力せよという命令
+    echo "Hello, world!";
+  ?>
+  <br>
+  <?php
+    // 7 * 2を計算して出力させる
+    //""や''で囲むと文字列として認識されるので注意
+    echo 7*2
+  ?>
+  <br>
+  <?php
+    //割り算の余りも計算できる
+    echo 8 % 3
+  ?>
+  <br>
+    //変数は$変数名=代入したい値;で定義
+    //数字や日本語から始まったり、ローマ字読みはNG
+    //英単語や、二単語以上は二つ目の単語の始まりを大文字にするとよい
+   <?php
+    echo $fruit="りんご";
+  ?>
+
+   <?php
+    $name = "にんじゃわんこ";
+    // "こんにちは！"という文字列と$nameを連結してechoする
+    $name = "こんにちは！{$name}";
+    echo $name;
+    //「.」記号で文字列と変数などを連結できる
+    $name1 = "にんじゃにゃんこ";
+    $name1 = "こんにちは".$name1;
+    echo $name1;
+  ?>
+　
+　<?php
+    $x = 99 * 99;
+    $y = 77 * 77;
+    // if文は()の内容がtrueのときだけ{}の内容が実行される
+    //==等しいとき、!=等しくないとき
+    if ($x>9800) {
+      echo "変数xは9800より大きいです。";
+    }//実行される
+
+    if ($y>6000) {
+      echo "変数yは6000より大きいです。";
+    }//実行されない   
+  ?>
+　　<?php
+    $age = 26;
+    if ($age>=30) {
+      echo "あなたは30歳以上です。";
+    }
+    //falseだった場合、elseの内容が実行される
+      else {
+        echo "あなたは30歳未満です。";
+      }
+    ?>
+    <?php
+    $remainder=2;
+    //switchはelseifと同じ使い方
+    //&&はand、||はor、!は否定の意味 ex: if($age>30 && $age<45)
+     switch ($remainder) {
+        case 0:
+        echo "大吉です。";
+        break;//breakがないと次のcaseにいってしまう
+        case 1:
+        echo "中吉です。";
+        break;
+        case 2:
+        echo "小吉です。";
+        break;
+            
+        default://どれにも一致しなかった場合
+        echo "凶です。";
+        break;
+    }
+?>
+
+</body>
+</html>
